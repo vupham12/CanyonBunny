9b2e469bb8373111fbdb5d875e11fb425ca18fe3
Your Name <you@example.com>
2021-09-16 17:29:38 +0700

    Thiet lap game canyon bunny

--- core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -1,31 +1,43 @@
 package com.packtpub.libgdx.canyonbunny;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	private static final String TAG =
+	        CanyonBunnyMain.class.getName();
+	    private WorldController worldController;
+	    private WorldRenderer worldRenderer;
+	    
+	    @Override 
+	    public void create() {
+	    	worldController = new WorldController();
+			worldRenderer = new WorldRenderer(worldController);
+	    }
+	    @Override
+	    public void render() {
+	    	worldController.update(Gdx.graphics.getDeltaTime());
+	    	Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+	    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    }
+	    @Override
+	    public void resize(int width, int height) {
+	    	worldRenderer.resize(width, height);
+	    }
+	    @Override
+	    public void pause() {
+	    	
+	    }
+	    @Override
+	    public void resume() {
+	    	
+	    }
+	    @Override
+	    public void dispose() {
+	    	
+	    }
+	 
+		 
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
-}
