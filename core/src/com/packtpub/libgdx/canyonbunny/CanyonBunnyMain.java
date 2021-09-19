d3b05be63dd864fbc1636dcf6d11a2c10864302f
Your Name <you@example.com>
2021-09-19 10:28:44 +0700

    Bai 05 doi tuong sprite

--- core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -15,24 +15,30 @@ public class CanyonBunnyMain extends ApplicationAdapter {
 	    	worldController = new WorldController();
 			worldRenderer = new WorldRenderer(worldController);
 	    }
+	    
 	    @Override
 	    public void render() {
 	    	worldController.update(Gdx.graphics.getDeltaTime());
 	    	Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
 	    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    	worldRenderer.render();
 	    }
+	    
 	    @Override
 	    public void resize(int width, int height) {
 	    	worldRenderer.resize(width, height);
 	    }
+	    
 	    @Override
 	    public void pause() {
 	    	
 	    }
+	    
 	    @Override
 	    public void resume() {
 	    	
 	    }
+	    
 	    @Override
 	    public void dispose() {
 	    	
---
+++ core/src/com/packtpub/libgdx/canyonbunny/Constants.java
@@ -0,0 +1,8 @@
+package com.packtpub.libgdx.canyonbunny;
+
+public class Constants {
+
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+
+}
